a871f3dab88e1139295c836e8f3ebaee127eb95f
ravicwp <109152895+ravicwp@users.noreply.github.com>
2022-07-25 13:59:56 +0530

    Update file2.tf

--- file2.tf
+++ file2.tf
@@ -6,3 +6,12 @@ resource "aws_s3_bucket" "mfadeletefile" {
     mfa_delete = true
   }
 }
+
+resource "aws_s3_bucket" "resourcefile1" {
+  bucket = "resourcefile1"
+  acl    = "private"
+  versioning {
+    enabled = true
+    mfa_delete = true
+  }
+}
